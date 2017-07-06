6e0f105bc04ad099b7be85d7aa47199a69f4095e
Richard McGarrigle <richard.mcgarrigle@gmail.com>
2017-07-06 22:13:25 +0100

    log intents

--- messages/index.js
+++ messages/index.js
@@ -33,6 +33,7 @@ const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' +
 var recognizer = new builder.LuisRecognizer(LuisModelUrl);
 var intents = new builder.IntentDialog({ recognizers: [recognizer] })
 console.log('intents', intents)
+/*
 .matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
 */
 
