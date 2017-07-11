bd99f9369ad659a1a25fd6b193c31713ebd7b948
richard & mark <sdfds@sdfds.com>
2017-07-11 13:26:18 +0100

    log args[0]

--- messages/index.js
+++ messages/index.js
@@ -39,7 +39,7 @@ var intents = new builder.IntentDialog({recognizers: [recognizer]})
         session.send('None intends matched for \'%s\'.', session.message.text);
     })
     .matches('None', function (session, args) {
-        session.send('None intends matched for \'%s\'.', args);
+        session.send('None intends matched for \'%s\'.', args[0]);
     })
     .onDefault((session) => {
         session.send('Sorry, I did not understand \'%s\'.', session.message.text);
